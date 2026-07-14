4bf3b6facbca7444a74bfc6f9214f602e2620faa
suriya22prakash <suriya2002prakash22@gmail.com>
2026-07-14 22:59:12 +0530

    added banner by suriya

---
+++ script.js
@@ -0,0 +1,23 @@
+var banner = document.getElementById("banner-container")
+var slider = document.getElementById("image-slider")
+var imglist = slider.querySelectorAll("img")
+var rhtbtn = document.getElementById("right")
+var lftbtn = document.getElementById("left")
+
+var index = 0
+var totalslides = imglist.length
+console.log(totalslides)
+function updateSlide() {
+    var slideWidth = banner.clientWidth
+    var offset = -index * slideWidth
+    slider.style.transform = `translateX(${offset}px)`
+}
+
+rhtbtn.addEventListener("click", function() {
+    index = (index + 1) % totalslides
+    updateSlide()
+})
+lftbtn.addEventListener("click", function() {
+    index = (index - 1 + totalslides) % totalslides
+    updateSlide()
+})
